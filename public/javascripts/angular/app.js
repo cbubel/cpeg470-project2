548f9f0608236f1b5c3d56c6d757b37686ac3979
Chris Bubel <christopher.s.bubel@gmail.com>
2016-03-19 13:40:19 -0400

    Restructure to static page serving

--- public/javascripts/angular/app.js
+++ public/javascripts/angular/app.js
@@ -1,3 +1,24 @@
-(function () {
-  var app = angular.module('PrimaryApp', []);
-})();
+var app = angular.module('PrimaryApp', [
+  'ngRoute',
+  'appControllers'
+]);
+
+app.config(['$routeProvider',
+function($routeProvider) {
+  $routeProvider.
+    when('/register', {
+      templateUrl: 'partials/register.html',
+      controller: 'UserController'
+    }).
+    when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'UserController'
+    }).
+    when('/change-password', {
+      templateUrl: 'partials/change-password.html',
+      controller: 'UserController'
+    }).
+    otherwise({
+      redirectTo: '/index'
+    });
+}]);
